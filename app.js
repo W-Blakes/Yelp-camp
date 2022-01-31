41ab9216a1f0d745a3d3a40c5684a7eaf2f857d5
Blake <swblakes@gmail.com>
2022-01-31 04:15:23 -0500

    local host edit

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://0.0.0.0:27017/yelp-camp';
 mongoose.connect(dbUrl);
 // mongodb://localhost:27017/yelp-camp
 // process.env.DB_URL
