dfccb73b402c96f9bcb279dbd03116bca0f6f097
Blake <swblakes@gmail.com>
2022-01-31 21:15:14 -0500

    try deploy

--- app.js
+++ app.js
@@ -74,10 +74,8 @@ const sessionConfig = {
 
 app.use(session(sessionConfig));
 app.use(flash());
-app.use(
-  helmet({ contentSecurityPolicy: false, crossOriginEmbedderPolicy: false })
-);
-
+app.use(helmet());
+// { contentSecurityPolicy: false, crossOriginEmbedderPolicy: false }
 const scriptSrcUrls = [
   'https://stackpath.bootstrapcdn.com/',
   'https://api.tiles.mapbox.com/',
